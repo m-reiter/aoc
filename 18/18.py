580755dd5e43845b4a3be836660e281e9988f87e
Manuel Reiter <m-reiter@users.noreply.github.com>
2020-12-18 12:11:34 +0100

    added day 18, part 2

--- 18/18.py
+++ 18/18.py
@@ -5,7 +5,7 @@ import re
 
 REMATCHTYPE = type(re.match("",""))
 
-def calculate(expression, precedence = ["+*"]):
+def calculate(expression, precedence = [r"+*"]):
 
   if isinstance(expression, REMATCHTYPE):
     expression = expression.group(1)
@@ -13,24 +13,37 @@ def calculate(expression, precedence = ["+*"]):
   operands = re.findall(r"\d+", expression)
   operators = re.findall(r"[+*]", expression)
 
-  result = operands.pop(0)
+  for current_operators in precedence:
 
-  for operator,operand in zip(operators,operands):
+    new_operands = [operands.pop(0)]
+    new_operators = []
+    
+    for operand,operator in zip(operands,operators):
+    
+      if operator in current_operators:
+      
+        new_operands.append(str(eval(new_operands.pop()+operator+operand)))
 
-    result = eval(str(result)+operator+operand)
+      else:
 
-  return(str(result))
+        new_operands.append(operand)
+        new_operators.append(operator)
 
-def reduce(expression):
+    operands = new_operands
+    operators = new_operators
+
+  return(operands[0])
+
+def reduce(expression, precedence = [r"+*"]):
 
   while "(" in expression:
-    expression = re.sub(r"\(([^()]+)\)", calculate, expression)
+    expression = re.sub(r"\(([^()]+)\)", lambda x: calculate(x, precedence), expression)
 
   return expression
 
-def evaluate(expression):
+def evaluate(expression, precedence = [r"+*"]):
 
-  return int(calculate(reduce(expression)))
+  return int(calculate(reduce(expression, precedence), precedence))
 
 def main():
   
@@ -39,6 +52,10 @@ def main():
   # part 1
 
   print(sum(evaluate(line) for line in input_))
+
+  # part 2
+
+  print(sum(evaluate(line, precedence = [r"+", r"*"]) for line in input_))
 
 if __name__ == "__main__":
   main()
