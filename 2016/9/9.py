215da77708a8f0944fab75dea488699fae466707
Manuel Reiter <m-reiter@users.noreply.github.com>
2023-08-19 13:42:25 +0200

    some optimisation

--- 2016/9/9.py
+++ 2016/9/9.py
@@ -22,17 +22,16 @@ def decompress(string):
 
 def get_length_v2(string):
   decompressed_length = 0
-  tokens = list(string)
-  while tokens:
-    token = tokens.pop(0)
-    if token == "(":
-      end = tokens.index(")")
-      length,times = map(int,ARGS.match("".join(tokens[:end])).groups())
-      expanded = "".join(tokens[end+1:end+1+length])
-      decompressed_length += times*get_length_v2(expanded)
-      tokens = tokens[end+1+length:]
-    else:
-      decompressed_length += 1
+  while "(" in string:
+    pos = string.index("(")
+    decompressed_length += pos
+    string = string[pos+1:]
+    end = string.index(")")
+    length,times = map(int,ARGS.match(string).groups())
+    expanded = string[end+1:end+1+length]
+    decompressed_length += times*get_length_v2(expanded)
+    string = string[end+1+length:]
+  decompressed_length += len(string)
   return decompressed_length
 
 def main():
