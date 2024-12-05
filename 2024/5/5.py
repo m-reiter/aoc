10bca4ddac03e37f956da149eb1d535bcb3ade9c
m-reiter <m-reiter@users.noreply.github.com>
2024-12-05 21:34:09 +0100

    day 5, part 2

--- 2024/5/5.py
+++ 2024/5/5.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 import fileinput
-from more_itertools import split_at
+from more_itertools import split_at, partition
+from functools import cmp_to_key
 
 def is_blank(line):
   return not(line.strip())
@@ -25,11 +26,25 @@ def get_middle(update):
     raise(ValueError("Even number of elements, no middle defined."))
   return update[l // 2]
 
+def cmp(a, b, rules):
+  if (a,b) in rules:
+    return -1
+  if (b,a) in rules:
+    return 1
+  return 0
+
 def main():
   rules, updates = get_input()
 
+  invalid, valid = partition(lambda x: is_valid(x, rules), updates)
+
   # part 1
-  print(sum(map(get_middle, [update for update in updates if is_valid(update, rules)])))
+  print(sum(map(get_middle, valid)))
+
+  # part 2
+  corrected = [sorted(update, key = cmp_to_key(lambda a,b: cmp(a, b, rules))) for update in invalid]
+  print(sum(map(get_middle, corrected)))
+
 
 if __name__ == "__main__":
   main()
