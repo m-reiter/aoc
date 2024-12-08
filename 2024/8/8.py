a4340f95dc165d5d54122720cf4feb46b1494e49
m-reiter <m-reiter@users.noreply.github.com>
2024-12-08 22:28:50 +0100

    Day 8, part 1, first try (blindly)

---
+++ 2024/8/8.py
@@ -0,0 +1,43 @@
+import fileinput
+from collections import defaultdict
+from itertools import combinations
+
+from P import P
+
+EMPTY = "."
+
+class Map:
+  def __init__(self, lines):
+    self.antennas = defaultdict(list)
+    self.antinodes = set()
+
+    for y, line in enumerate(lines):
+      for x, symbol in enumerate(line.strip()):
+        if symbol != EMPTY:
+          self.antennas[symbol].append(P(x,y))
+
+    self.width = x
+    self.height = y
+
+  def __contains__(self, point):
+    return (0 <= point.x < self.width and
+            0 <= point.y < self.height)
+
+  def find_antinodes(self):
+    for coordinates in self.antennas.values():
+      for a, b in combinations(coordinates, 2):
+        offset = b - a
+        for point in (a - offset, b + offset):
+          if point in self:
+            self.antinodes.add(point)
+
+def main:
+  area_map = Map(fileinput.input())
+
+  # part 1
+  area_map.find_antinodes()
+  print(len(area_map.antinodes))
+
+if __name__ == "__main__":
+  main()
+
