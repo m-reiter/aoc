1297b94a2b5594a73c34da62bc982b12b883e2c8
Manuel Reiter <Manuel.Reiter@dwd.de>
2024-12-16 17:53:29 +0100

    day 10, part 2

--- 2024/10/10.py
+++ 2024/10/10.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import fileinput
 from collections import defaultdict
 
@@ -7,6 +9,7 @@ class Map:
     def __init__(self, inputdata):
         self.tiers = defaultdict(list)
         self.reachable = defaultdict(set)
+        self.distinct_trails = defaultdict(list)
         for y, line in enumerate(inputdata):
             for x, char in enumerate(line.strip()):
                 self.tiers[int(char)].append(P(x, y))
@@ -20,16 +23,24 @@ class Map:
                     if n in self.tiers[tier - 1]:
                         if tier == 9:
                             self.reachable[n].add(p)
+                            self.distinct_trails[n].append([ p ])
                         else:
                             self.reachable[n] |= self.reachable[p]
+                            self.distinct_trails[n].extend([[ p ] + trail for trail in self.distinct_trails[p]])
             tier -= 1
+#            for k in sorted(self.distinct_trails.keys()):
+#                if k in self.tiers[tier]:
+#                    print(k, self.distinct_trails[k])
                 
 def main():
     m = Map(fileinput.input())
+    m.find_trails()
     
     # part 1
-    m.find_trails()
     print(sum(len(m.reachable[p]) for p in m.tiers[0]))
+
+    # part 2
+    print(sum(len(m.distinct_trails[p]) for p in m.tiers[0]))
 
 if __name__ == "__main__":
     main()
