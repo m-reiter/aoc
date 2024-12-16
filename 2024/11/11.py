f5ca81c08c3a3bdd7ef4f952884e0194459bc47f
m-reiter <m-reiter@users.noreply.github.com>
2024-12-16 23:04:15 +0100

    day 11, part 2

--- 2024/11/11.py
+++ 2024/11/11.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
 
 import fileinput
-from collections import defaultdict
+from collections import Counter, defaultdict
+from functools import cache, lru_cache
 
+#@lru_cache(maxsize = 1)
+@cache
 def blink(stone):
     if stone == 0:
-        return [ 1 ]
-    if (l := len(s := str(stone))) % 2 == 0:
-        return list(map(int, [s[:l // 2], s[l // 2:]]))
-    return [ stone * 2024 ]
+        next_gen = [ 1 ]
+    elif (l := len(s := str(stone))) % 2 == 0:
+        next_gen = list(map(int, [s[:l // 2], s[l // 2:]]))
+    else:
+        next_gen = [ stone * 2024 ]
+    return next_gen
 
-def blink_n_times(stones, n):
-    for _ in range(n):
-        print(_)
-        new_stones = []
-        for stone in stones:
-            new_stones.extend(blink(stone))
-        stones = new_stones
+def evolve(stones, generations):
+    #return sum(([ blink_n_times(stone, generations) ] * n for stone, n in Counter(stones)), [])
+    if not isinstance(stones, dict):
+        stones = Counter(stones)
+    for _ in range(generations):
+        steps = [ blink(stone) for stone in stones ]
+        next_gen = defaultdict(int)
+        for result, n in zip(steps, stones.values()):
+            for stone in result:
+                next_gen[stone] += n
+        stones = next_gen
     return stones
 
 def main():
@@ -26,12 +35,12 @@ def main():
     stones = list(map(int, line.strip().split()))
 
     # part 1
-    stones = blink_n_times(stones, 25)
-    print(len(stones))
+    stones = evolve(stones, 25)
+    print(sum(stones.values()))
 
     # part 2
-    stones = blink_n_times(stones, 50)
-    print(len(stones))
+    stones = evolve(stones, 50)
+    print(sum(stones.values()))
 
 if __name__ == "__main__":
     main()
