39be3462dc0d039ffc67402cef7e7555479bd91e
m-reiter <m-reiter@users.noreply.github.com>
2024-12-28 12:04:19 +0100

    day 16, part 2 (also solves part 1, but much slower)

--- 2024/16/16.py
+++ 2024/16/16.py
@@ -33,21 +33,34 @@ class ReindeerMaze(dict):
           self.end = P(x,y)
     self.best_scores = defaultdict(int)
     self.best_score = None
+    self.best_paths = defaultdict(list)
+    self.best_seats = { self.start, self.end }
 
   def find_paths(self):
-    self.best_scores = defaultdict(int)
     self.best_scores[(self.start, EAST)] = 0
+    self.best_paths[(self.start, EAST)].append([])
     to_check = deque([ (self.start, EAST, 0) ]) # tuples are (position, facing, score)
 
     while to_check:
       position, facing, score = to_check.popleft()
       for neighbor, new_facing, new_score in [ (position + facing, facing, score + 1) ] + [ (position, turn, score + 1000) for turn in TURNS[facing] ]:
-        if self[neighbor] != WALL and ((neighbor, new_facing) not in self.best_scores or new_score < self.best_scores[(neighbor, new_facing)]):
-          self.best_scores[(neighbor, new_facing)] = new_score
-          if neighbor != self.end:
-            to_check.append((neighbor, new_facing, new_score))
+        is_new = (neighbor, new_facing) not in self.best_scores
+        if not is_new:
+          previous_best = self.best_scores[(neighbor, new_facing)]
+        if self[neighbor] != WALL and (is_new or new_score <= previous_best):
+          new_paths = [ previous + [ position ] for previous in self.best_paths[(position, facing)] ]
+          if is_new or new_score < previous_best:
+            self.best_scores[(neighbor, new_facing)] = new_score
+            self.best_paths[(neighbor, new_facing)] = new_paths
+            if neighbor != self.end:
+              to_check.append((neighbor, new_facing, new_score))
+          else:
+            self.best_paths[(neighbor, new_facing)].extend(new_paths)
 
     self.best_score = min(score for (position, facing), score in self.best_scores.items() if position == self.end)
+    for situation in [ (position, facing) for (position, facing), score in self.best_scores.items() if position == self.end and score == self.best_score ]:
+      for path in self.best_paths[situation]:
+        self.best_seats.update(path)
 
 def main():
   maze = ReindeerMaze(fileinput.input())
@@ -55,6 +68,9 @@ def main():
   # part 1
   maze.find_paths()
   print(maze.best_score)
+
+  # part 2
+  print(len(maze.best_seats))
 
 if __name__ == "__main__":
   main()
