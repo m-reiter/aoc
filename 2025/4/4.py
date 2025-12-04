7aad2552776cccaee6d629d9772666be31f192bb
m-reiter <m-reiter@users.noreply.github.com>
2025-12-04 08:40:12 +0100

    day 4, part 2, somewhat slow (~ 5 seconds)

--- 2025/4/4.py
+++ 2025/4/4.py
@@ -23,8 +23,9 @@ class Grid(dict):
     )
 
   def show_accessibles(self):
+    accessibles = self.get_accessibles()
     return "\n".join(
-      "".join(EMPTY if self.is_empty(P(x,y)) else "x" if self.is_accessible(P(x,y)) else PAPER_ROLL for x in range(self.size.x))
+      "".join(EMPTY if self.is_empty(P(x,y)) else "x" if P(x,y) in accessibles else PAPER_ROLL for x in range(self.size.x))
       for y in range(self.size.y)
     )
 
@@ -39,14 +40,47 @@ class Grid(dict):
       return False
     return sum(self.is_roll(n) for n in position.get_neighbors(borders = self.size-P(1,1))) < 4
 
+  def get_accessibles(self):
+    return [ roll for roll in self.keys() if self.is_accessible(roll) ]
+
   def count_accessibles(self):
-    return sum(self.is_accessible(roll) for roll in self.keys())
+    return len(self.get_accessibles())
+
+  def remove(self, positions):
+    for p in positions:
+      del self[p]
 
 def main():
   grid = Grid(fileinput.input())
   
   # part 1
   print(grid.count_accessibles())
+
+  # part 2
+  verbose = grid.size.x < 20
+
+  if verbose:
+    print()
+    print("Initial state:")
+    print(grid)
+    print()
+
+  removed = 0
+
+  while len(removables := grid.get_accessibles())  > 0:
+    if verbose:
+      print(f"Remove {len(removables)} rolls of paper:")
+      print(grid.show_accessibles())
+      print()
+    grid.remove(removables)
+    removed += len(removables)
+
+  if verbose:
+    print("Final state:")
+    print(grid)
+    print()
+
+  print(removed)
 
 if __name__ == "__main__":
   main()
