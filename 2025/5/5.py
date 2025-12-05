219f874d9685f08948e6ce2c99d731246954b7a2
Manuel Reiter <Manuel.Reiter@dwd.de>
2025-12-05 16:39:53 +0100

    day 5, part 2

--- 2025/5/5.py
+++ 2025/5/5.py
@@ -8,14 +8,25 @@ def is_blank(line):
 
 def main():
   ranges, ingredients = split_at(fileinput.input(), is_blank)
-  fresh_ranges = []
+  fresh_ranges = set()
   for r in ranges:
     start, stop = map(int, r.split("-"))
-    fresh_ranges.append(range(start, stop + 1))
+    fresh_ranges.add(range(start, stop + 1))
   ingredients = list(map(int, ingredients))
 
+  # consolidate ranges
+  consolidated = { fresh_ranges.pop() }
+  for r in fresh_ranges:
+    overlapping = { o for o in consolidated if r.start <= o.stop and r.stop >= o.start }
+    consolidated -= overlapping
+    overlapping.add(r)
+    consolidated.add(range(min(o.start for o in overlapping), max(o.stop for o in overlapping)))
+
   # part 1
-  print(sum(any(i in r for r in fresh_ranges) for i in ingredients))
+  print(sum(any(i in r for r in consolidated) for i in ingredients))
+
+  # part 2
+  print(sum(len(r) for r in consolidated))
 
 if __name__ == "__main__":
   main()
