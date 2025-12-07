55fdae2fe0a417c093fe0576ffdbdde3df1b3181
m-reiter <m-reiter@users.noreply.github.com>
2025-12-07 20:06:39 +0100

    2015, day 23, part2, takes somewhat long (~30 sec)

--- 2015/24/24.py
+++ 2015/24/24.py
@@ -6,26 +6,31 @@ from functools import reduce
 from operator import mul
 
 def balance(weights, ngroups):
-  pass
-  
-def main():
-  weights = set(map(int, fileinput.input()))
-  target_weight, remainder = divmod(sum(weights), 3)
+  target_weight, remainder = divmod(sum(weights), ngroups)
   assert remainder == 0
 
-  for i in range(len(weights)):
+  for i in range(1, len(weights)):
     combis = [ set(c) for c in combinations(weights, i) if sum(c) == target_weight ]
+    if combis and ngroups == 2:
+      return combis
     valid_combis = []
     for combi in combis:
       remainder = weights - combi
-      for j in range(len(remainder)):
-        seconds = [ set(c) for c in combinations(remainder, j) if sum(c) == target_weight ]
-        if seconds:
-          valid_combis.append(combi)
-          break
+      if balance(remainder, ngroups - 1):
+        valid_combis.append(combi)
     if valid_combis:
-      break
+      return valid_combis
+  
+def main():
+  weights = set(map(int, fileinput.input()))
 
+  # part 1
+  valid_combis = balance(weights, 3)
+  entanglements = [ reduce(mul, combi) for combi in valid_combis ]
+  print(min(entanglements))
+
+  # part 2
+  valid_combis = balance(weights, 4)
   entanglements = [ reduce(mul, combi) for combi in valid_combis ]
   print(min(entanglements))
 
